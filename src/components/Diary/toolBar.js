252bcb78191781a60ccfb30c86b8e11c4a183686
Jordan McCollam <jordy.mccollam@gmail.com>
2021-02-26 12:46:26 -0500

    Added calendar tool btn. No function yet

--- src/components/Diary/toolBar.js
+++ src/components/Diary/toolBar.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { Container, Row, Col, Form, Button, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button, OverlayTrigger, Tooltip, Dropdown } from 'react-bootstrap';
 import * as Diary from '../Diary';
-import { BsPencil, BsChevronUp, BsChevronDown, BsClock, BsChevronBarExpand } from 'react-icons/bs';
+import { BsPencil, BsChevronUp, BsChevronDown, BsClock, BsChevronBarExpand, BsCalendar } from 'react-icons/bs';
 import { FaPaperPlane } from 'react-icons/fa';
-import { VscSmiley } from 'react-icons/vsc';
+import { VscSmiley, VscFilter } from 'react-icons/vsc';
 import { HiOutlineDotsHorizontal } from 'react-icons/hi';
 
 const toolSize = 24;
@@ -13,46 +13,74 @@ const ToolBar = (props) => {
 
     const tools = [
         {
-            icon: <BsChevronUp size={toolSize} />,
+            icon: <BsChevronUp size={toolSize}/>,
             action: () => setCollapseAllTrigger(old => !old),
             label: 'Collapse All'
         },
         {
-            icon: <BsChevronUp size={toolSize} />,
-            action: () => setCollapseAllTrigger(old => !old),
-            label: 'Collapse All'
-        },
-        {
-            icon: <BsChevronUp size={toolSize} />,
-            action: () => setCollapseAllTrigger(old => !old),
-            label: 'Collapse All'
-        },
-        {
-            icon: <BsChevronUp size={toolSize} />,
-            action: () => setCollapseAllTrigger(old => !old),
-            label: 'Collapse All'
+            icon: <div style={{fontSize: 14}}>{'02/12/test'} <BsCalendar size={toolSize} className="mb-1 ml-2 text-primary"/></div>,
+            label: 'Filter Entries',
+            dropdown: [
+                {
+                    label: 'Action',
+                    action: () => console.log("Action")
+                }
+            ]
         },
     ]
 
     return (
         <Row className="px-3 justify-content-between">
             {tools.map((tool, index) => {
-                return (
-                    <Col key={`tool-${index}`} >
-                        <OverlayTrigger
-                            placement='top'
-                            overlay={
-                                <Tooltip id={`tool-tooltip-${index}`}>
-                                    {tool.label}
-                                </Tooltip>
-                            }
-                        >
-                            <div className="card py-2 card-btn" onClick={tool.action}>
-                                {tool.icon}
-                            </div>
-                        </OverlayTrigger>
-                    </Col>
-                )
+                if (tool.dropdown) {
+                    return (
+                        <Col key={`tool-${index}`} >
+                            <OverlayTrigger
+                                placement='top'
+                                overlay={
+                                    <Tooltip id={`tool-tooltip-${index}`}>
+                                        {tool.label}
+                                    </Tooltip>
+                                }
+                            >
+                                <Dropdown>
+                                    <Dropdown.Toggle DropdownIndicator variant="clear" className="dropdown-card-btn" style={{height: 47, width: '100%'}}>
+                                        <div className="card py-2 card-btn" onClick={tool.action}>
+                                            {tool.icon}
+                                        </div>
+                                    </Dropdown.Toggle>
+                                
+                                    <Dropdown.Menu>
+                                        {tool.dropdown.map((toolItem, tIndex) => (
+                                            <Dropdown.Item onClick={toolItem.action} key={`tool-item-${tIndex}`}>{toolItem.label}</Dropdown.Item>
+                                        ))}
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                            </OverlayTrigger>
+                        </Col>
+                    )
+                }
+                else if (tool.action) {
+                    return (
+                        <Col key={`tool-${index}`} >
+                            <OverlayTrigger
+                                placement='top'
+                                overlay={
+                                    <Tooltip id={`tool-tooltip-${index}`}>
+                                        {tool.label}
+                                    </Tooltip>
+                                }
+                            >
+                                <div className="card py-2 card-btn" onClick={tool.action}>
+                                    {tool.icon}
+                                </div>
+                            </OverlayTrigger>
+                        </Col>
+                    )
+                }
+                else {
+                    return <></>
+                }
             })}
         </Row>
     )
