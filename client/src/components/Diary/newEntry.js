544c4539861574ce1e8c9a9b9ddb111d3ab7ed95
Jordan McCollam <jordy.mccollam@gmail.com>
2021-02-27 16:09:22 -0500

    Can change date for new entry now

--- client/src/components/Diary/newEntry.js
+++ client/src/components/Diary/newEntry.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import { Row, Col, Form, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Row, Col, Form, Button, Dropdown } from 'react-bootstrap';
 import * as Diary from '../Diary';
+import moment from 'moment';
+import { Calendar } from 'react-date-range';
 import { BsPencil } from 'react-icons/bs';
 import { FaPaperPlane } from 'react-icons/fa';
 import { VscSmiley } from 'react-icons/vsc';
 
+const dateFormat = 'M/DD/YY';
+
 const NewEntry = (props) => {
     const { collapsed, setCollapsed } = props;
+    const [ date, setDate ] = useState(moment(new Date()).format(dateFormat));
+
+    const changeDate = (_date) => {
+        console.log("changeDate", _date);
+        setDate(moment(_date).format(dateFormat));
+    }
+    const resetDate = () => {
+        setDate(moment(new Date()).format(dateFormat));
+    }
 
     return (
         <Row className="px-3">
@@ -27,9 +40,25 @@ const NewEntry = (props) => {
                         <>
                             <div className="entry-header">
                                 <Form.Label>New Entry <BsPencil/></Form.Label>
-                                <Diary.Collapse collapsed={collapsed} setCollapsed={setCollapsed} />
+                                <div className="d-flex align-items-center">
+                                    <Dropdown className="new-entry-date-selector">
+                                        <Dropdown.Toggle DropdownIndicator variant="white" className="text-secondary">
+                                            Date of post: {date}
+                                        </Dropdown.Toggle>
+                                    
+                                        <Dropdown.Menu>
+                                            <Calendar 
+                                                value={new Date(date)}
+                                                onChange={changeDate}
+                                                color={'red'}
+                                            />
+                                            <Button block variant="white" onClick={resetDate} >Reset</Button>
+                                        </Dropdown.Menu>
+                                    </Dropdown>
+                                    <Diary.Collapse collapsed={collapsed} setCollapsed={setCollapsed} />
+                                </div>
                             </div>
-                            <div className="border-bottom pb-3 mb-3">
+                            <div className="new-entry-content" >
                                 <Form.Control as="textarea" rows={3} />
                                 <Button variant="primary" block className="btn-sm form-btn">Submit <FaPaperPlane/></Button>
                             </div>
