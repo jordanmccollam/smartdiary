8c136f1f1d586ec4c821a143ed7b1b71908630d1
Jordan McCollam <jordy.mccollam@gmail.com>
2021-02-28 02:04:20 -0500

    Removed some test logs

--- client/src/index.js
+++ client/src/index.js
@@ -11,10 +11,6 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
-console.log("DOMAIN", domain);
-console.log("ID", clientId);
-console.log("TEST", process.env);
-
 ReactDOM.render(
   <Auth0Provider
     domain={domain}
