abe630783b82aadf8ce0129103358391f9f2d54d
Jordan McCollam <jordy.mccollam@gmail.com>
2021-03-04 13:39:17 -0500

    Added a catch to axios requests

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,23 +1,23 @@
 import axios from 'axios';
 
 // USERS
-export const getUser = email => axios.get(`/api/user/${email}`);
-export const createUser = payload => axios.post(`/api/user`, payload);
-export const updateUser = (id, payload) => axios.put(`/api/user/${id}`, payload);
+export const getUser = email => axios.get(`/api/user/${email}`).catch(e => console.error("request", e));
+export const createUser = payload => axios.post(`/api/user`, payload).catch(e => console.error("request", e));
+export const updateUser = (id, payload) => axios.put(`/api/user/${id}`, payload).catch(e => console.error("request", e));
 
 // ENTRIES
-export const getEntries = () => axios.get(`/api/entries`);
-export const getEntry = id => axios.get(`/api/entry/${id}`);
-export const createEntry = payload => axios.post(`/api/entry`, payload);
-export const updateEntry = (id, payload) => axios.put(`/api/entry/${id}`, payload);
-export const deleteEntry = id => axios.delete(`/api/entry/${id}`);
+export const getEntries = () => axios.get(`/api/entries`).catch(e => console.error("request", e));
+export const getEntry = id => axios.get(`/api/entry/${id}`).catch(e => console.error("request", e));
+export const createEntry = payload => axios.post(`/api/entry`, payload).catch(e => console.error("request", e));
+export const updateEntry = (id, payload) => axios.put(`/api/entry/${id}`, payload).catch(e => console.error("request", e));
+export const deleteEntry = id => axios.delete(`/api/entry/${id}`).catch(e => console.error("request", e));
 
 // MOODS
-export const getMoods = () => axios.get(`/api/moods`);
-export const getMood = id => axios.get(`/api/mood/${id}`);
-export const createMood = payload => axios.post(`/api/mood`, payload);
-export const updateMood = (id, payload) => axios.put(`/api/mood/${id}`, payload);
-export const deleteMood = id => axios.delete(`/api/mood/${id}`);
+export const getMoods = () => axios.get(`/api/moods`).catch(e => console.error("request", e));
+export const getMood = id => axios.get(`/api/mood/${id}`).catch(e => console.error("request", e));
+export const createMood = payload => axios.post(`/api/mood`, payload).catch(e => console.error("request", e));
+export const updateMood = (id, payload) => axios.put(`/api/mood/${id}`, payload).catch(e => console.error("request", e));
+export const deleteMood = id => axios.delete(`/api/mood/${id}`).catch(e => console.error("request", e));
 
 const apis = {
     createEntry,
